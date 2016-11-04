a28c8ef9a0d34af9decb6e0e9f2612dc88303ad1
Luis Crespo <lcrespo@es.ibm.com>
2016-11-04 12:24:13 +0100

    Testing with express

---
+++ src/svr/index.ts
@@ -0,0 +1,45 @@
+let server = require('http').createServer();
+let url = require('url');
+let WebSocketServer = require('ws').Server;
+
+let express = require('express');
+let app = express();
+let port = 1337;
+
+// -------------------- HTTP server --------------------
+let index_html = `
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>WebSockets chat</title>
+	<script src="https://code.jquery.com/jquery-3.0.0.js"></script>
+</head>
+<body>
+	Welcome to ws-chat
+</body>
+</html>
+`;
+
+app.use(function (req, res) {
+	res.statusCode = 200;
+	res.setHeader('Content-Type', 'text/html');
+	res.end(index_html);
+});
+
+// -------------------- WS server --------------------
+let wss = new WebSocketServer({ server });
+wss.on('connection', function connection(ws) {
+	let location = url.parse(ws.upgradeReq.url, true);
+	console.log('connection from', location);
+
+	ws.on('message', function incoming(message) {
+		console.log('received: %s', message);
+	});
+	ws.send('hello');
+});
+
+// -------------------- Server startup --------------------
+server.on('request', app);
+server.listen(port, _ => {
+	console.log('Listening on ' + server.address().port);
+});
